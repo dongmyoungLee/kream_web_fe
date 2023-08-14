0773a7d6428ed0c48f62aaca3f97e7880b06ad08
donymyounglee <pajang1515@daum.net>
2023-08-14 14:14:30 +0900

    humanResource pagenation..8개..

--- src/components/pages/Layout/HumanResources.js
+++ src/components/pages/Layout/HumanResources.js
@@ -80,14 +80,18 @@ const HumanResources = () => {
       case 'Nike' :
         setFilterJobList(humanResourcesDevJob);
 
-        axios.get('http://localhost:8080/api/v1/products').then((res) => {
-          if (res.status === 200) {
-            setProductList(res.data);
+        axios.get('http://localhost:8080/api/v1/products', {
+          params: {
+            page: 0,
+            size: 8
           }
-        })
-        .catch((err) => {
+        }).then((res) => {
+          if (res.status === 200) {
+            setProductList(res.data.content);
+          }
+        }).catch((err) => {
           console.log(err);
-        })
+        });
 
       break;
       case 'Adidas' :
